76b74832b8c96b143f0543c7cf0cdb1ac2eec372
Gergosz-2000 <gergoszivak@gmail.com>
2022-01-20 12:18:00 +0100

    login müködik

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { AuthService } from '../shared/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  loginForm!: FormGroup
+
+  constructor(
+    private auth:AuthService,
+    private formBuilder:FormBuilder
+    ) { }
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      user: [''],
+      pass: ['']
+    });
+  }
+
+  login(){
+    let user = this.loginForm.value.user;
+    let pass = this.loginForm.value.pass;
+
+    this.auth.login(user,pass)
+    .subscribe(res => {
+      console.log(res.success);
+      if(res.succes){
+        localStorage.setItem('currentUser', 
+        JSON.stringify({token: res.data.token, name: res.data.name}));
+      }
+    });
+  }
+
+}
